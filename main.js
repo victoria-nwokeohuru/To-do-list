6748893ebe31c1e36170a7731b23fd1db6da7ca2
victoria-nwokeohuru <vickynewman2012@gmail.com>
2020-11-15 17:17:39 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
-//getting our objects
-let input = document.querySelector("#input");
-let  button = document.querySelector("#button");
-let list = document.querySelector("#list");
-let el = document.getElementsByTagName('li');
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const list = document.querySelector('#list');
 
-// function that will allow adding elements to lists on clicking
-button.addEventListener('click',() => {
-    let txt = input.value;
-    if(txt ==''){
-        alert('write something');
-    }else{
-        li = document.createElement('li');
-    li.innerHTML = txt;
-    list.insertBefore(li,list.childNodes[0]);
-    input.value = '';
+const storage = localStorage.getItem("todoList");
+
+if(storage) {
+    list.innerHTML = storage;
+}
+
+form.addEventListener('submit', function(ev){
+    ev.preventDefault();
+    if (input.value.length > 0 ) {
+        const li = document.createElement('li');
+        li.classList.add("listText");
+        const listContent = `${input.value}<span class="close">\u00D7</span>`;
+        li.innerHTML = listContent;
+        list.appendChild(li);
+        input.value = '';
     }
-    
-},)
 
+    localStorage.setItem("todoList", list.innerHTML);
+});
 
-//this function will allow us to check the clicked elements
-list.addEventListener('click', e => {
-    if(e.target.tagName == 'li'){
-         e.target.classList.toggle('checked');
+list.addEventListener('click', function(ev){
+    console.log(ev);
+    if (ev.target.classList.contains("listText")) {
+        ev.target.classList.toggle('checked');      
     }
+
+    if (ev.target.classList.contains('close')) {
+        list.removeChild(ev.target.parentElement); 
+    }
+
+    localStorage.setItem("todoList", list.innerHTML);
 })
-
-
-
